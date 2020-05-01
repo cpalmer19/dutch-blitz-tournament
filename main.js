ec81495f7a58a5bacca98da6f8abbb8575fbde0f
Craig Palmer <palmer.craig1@gmail.com>
2020-05-01 11:56:05 -0700

    Add data persistance and refactor a bit.

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
+
+var gameData = {
+    players: [],
+    rounds: [],
+    date: Date.now()
+};
+
 $(() => {
     // generate options for selecting number of players
     const select = $('#numPlayerSelect');
@@ -7,10 +14,19 @@ $(() => {
 
     // set change and click handlers
     select.change(handlePlayerNumChange);
-    $('#btnStartTournament').click(generateTournament);
+    $('#divRounds').change(handleScoreChange);
+    $('#btnStartTournament').click(handleStartTournamentButton);
     $('#btnClearTournament').click(handleClearButton);
     
-    resetData();
+    // load saved game if present, otherwise clear the page
+    gameData = loadSavedGameData();
+    if (gameData) {
+        loadRounds();
+        loadPlayers();
+        startTournament();
+    } else {
+        clearTournament();
+    }
 });
 
 /**
@@ -18,7 +34,7 @@ $(() => {
  * Takes player names from the Players table, and generates unique pairings
  * for each round.
  */
-function generateTournament() {
+function handleStartTournamentButton() {
     const playerNames = getPlayerNames();
 
     // verify all players have unique names
@@ -27,21 +43,70 @@ function generateTournament() {
         return;
     }
 
-    // find pairings for each round
-    const rounds = generateRounds(playerNames.length);
+    gameData.players = playerNames;
+    gameData.rounds = generateRoundPairings(playerNames.length);
     
-    // show the pairings for each round
-    const roundsHtml = createRoundTables(rounds, playerNames);
+    loadRounds();
+    startTournament();
+}
+
+/**
+ * Handle the 'Clear' button being pressed.
+ * Make the user confirm so data is not accidentally lost.
+ */
+function handleClearButton() {
+    if (confirm("Clear all tournament data?")) {
+        clearTournament();
+    }
+}
+
+/**
+ * Handle when the number of players is changed in the 'select' box.
+ * It clears the player table and creates the appropriate number of rows.
+ * @param {Event} event - the triggered change event
+ */
+function handlePlayerNumChange(event) {
+    const numPlayers = parseInt(event.target.value);
+
+    const playerTable = $('#playerTableBody');
+    playerTable.html('');
+
+    for (let i = 0; i < numPlayers; i++) {
+        playerTable.append(`<tr>
+            <td><input type="text" id="playerName${i}" /></td>
+            <td class="playerTotal" id="playerTotal${i}"></td>
+        </tr>`);
+    }
+}
+
+/**
+ * Generate tables for the pairings for each round based on the current gameData.
+ */
+function loadRounds() {
+    let roundsHtml = '<hr/>';
+    for (let i in gameData.rounds) {
+        roundsHtml += createRoundTable(i);
+    }
     $('#divRounds').html(roundsHtml);
+}
 
-    // assign the on-change handler for new score inputs
-    $('.roundScore').change(handleScoreChange);
+/**
+ * Show the player setup based on the current gameData
+ */
+function loadPlayers() {
+    const players = Array.from(gameData.players);
+    
+    const selfIndex = players.indexOf('self');
+    if (selfIndex != -1) {
+        players.splice(selfIndex, 1);
+    }
 
-    // disable changing number of players
-    $('#numPlayerSelect').prop("disabled", true);
-
-    // disable editing the player names
-    $('#playerTableBody').find('input').prop("disabled", true);
+    $('#numPlayerSelect').val(players.length).change();
+    for (let i = 0; i < players.length; i++) {
+        $('#playerName' + i).val(players[i]);
+        updatePlayerScore(i);
+    }
+    sortPlayersByScore();
 }
 
 /**
@@ -95,9 +160,9 @@ function strIsBlank(str) {
 /**
  * Generate the tournament schedule for the given number of players.
  * @param {Int} numPlayers - the number of players in the tournament
- * @returns {Array[Array[Array[Int]]]} the pairings for each round
+ * @returns {Array[Array[Object]]} the pairings for each round
  */
-function generateRounds(numPlayers) {
+function generateRoundPairings(numPlayers) {
     // generate array of 0..numPlayers
     const playerNums = [];
     for (let i = 0; i < numPlayers;  i++) {
@@ -111,7 +176,12 @@ function generateRounds(numPlayers) {
         // pair 0 to N, 1 to N-1, 2 to N-2
         const firstHalf = playerNums.slice(0, numPlayers/2);
         const secondHalf = playerNums.slice(numPlayers/2).reverse();
-        const pairs = firstHalf.map((e, i) => [e, secondHalf[i]]);
+        const pairs = firstHalf.map((e, i) => {
+            return {
+                players: [e, secondHalf[i]],
+                score: ''
+            };
+        });
         rounds.push(pairs);
 
         // rotate playerNums, keeping the first number the same
@@ -126,53 +196,38 @@ function generateRounds(numPlayers) {
 }
 
 /**
- * Create the round tables for the given rounds and players.
- * @param {Array[Array[Array[Int]]]} rounds - pairings for each round
- * @param {Array[String]} playerNames
- * @returns {String} the html for the round tables
- */
-function createRoundTables(rounds, playerNames) {
-    let html = '<hr/>';
-    for (let i in rounds) {
-        html += createRoundTable(i, rounds[i], playerNames);
-    }
-    return html;
-}
-
-/**
- * Create the table for the specified round. Each pair gets its own row with
- * an input for their score.s
+ * Create the table for the specified round within the currency gameData.
+ * Each pair gets its own row with an input for their score.
  * @param {Int} roundNum - the round number
- * @param {Array[Array[Int]]} pairings - player pairings by number
- * @param {Array[String]} playerNames - player names as strings
  * @returns {String} the html for the new round table
  */
-function createRoundTable(roundNum, pairings, playerNames) {
+function createRoundTable(roundNum) {
     const humanRoundNum = parseInt(roundNum) + 1;
     let html = '<h2>Round ' + humanRoundNum + '</h2>' +
-            '<table>' +
+            '<table data-round="' + roundNum + '">' +
                 '<tr><th>Pair</th><th>Score</th></tr>';
 
     // For odd-number of players, one person plays by themself each round.
     // 'self' is a placeholder for such a scenario.
-    const selfIndex = playerNames.indexOf('self');
+    const selfIndex = gameData.players.indexOf('self');
     const isSinglePair = (pair) => pair.includes(selfIndex);
 
-    for (let pair of pairings) {
+    for (let pairing of gameData.rounds[roundNum]) {
         let pairNames, classes;
 
+        let pair = pairing.players;
         if (isSinglePair(pair)) {
             const playerIndex = pair[0] == selfIndex ? pair[1] : pair[0];
-            pairNames = playerNames[playerIndex];
+            pairNames = gameData.players[playerIndex];
             classes = 'scorePlayer' + playerIndex;
         } else {
-            pairNames = playerNames[pair[0]] + ' and ' + playerNames[pair[1]];
+            pairNames = gameData.players[pair[0]] + ' and ' + gameData.players[pair[1]];
             classes = 'scorePlayer' + pair[0] + ' scorePlayer' + pair[1];
         }
 
         html += `<tr>
             <td>${pairNames}</td>
-            <td><input type="text" class="roundScore ${classes}" size="5"/></td>
+            <td><input type="text" class="roundScore ${classes}" size="5" value="${pairing.score}"/></td>
         </tr>`;
     }
 
@@ -181,21 +236,36 @@ function createRoundTable(roundNum, pairings, playerNames) {
 }
 
 /**
- * Handle when the number of players is changed in the 'select' box.
- * It clears the player table and creates the appropriate number of rows.
- * @param {Event} event - the triggered change event
+ * Start the tournament by disabling the setup.
  */
-function handlePlayerNumChange(event) {
-    const numPlayers = parseInt(event.target.value);
+function startTournament() {
+    // disable changing number of players
+    $('#numPlayerSelect').prop('disabled', true);
 
-    const playerTable = $('#playerTableBody');
-    playerTable.html('');
+    // disable editing the player names
+    $('#playerTableBody').find('input').prop('disabled', true);
 
-    for (let i = 0; i < numPlayers; i++) {
-        playerTable.append(`<tr>
-            <td><input type="text" id="playerName${i}" /></td>
-            <td class="playerTotal" id="playerTotal${i}"></td>
-        </tr>`);
+    // disable the 'start tournament' button
+    $('#btnStartTournament').prop('disabled', true);
+}
+
+/**
+ * Clear all the tournament values.
+ */
+function clearTournament() {
+    $('#divRounds').html('');
+    $('#playerTableBody').html('');
+    $('#numPlayerSelect').change();
+
+    // enable setup inputs
+    $('#numPlayerSelect').prop("disabled", false);
+    $('#btnStartTournament').prop("disabled", false);
+
+    localStorage.removeItem("gameData");
+    gameData = {
+        players: [],
+        rounds: [],
+        date: Date.now()
     }
 }
 
@@ -204,51 +274,29 @@ function handlePlayerNumChange(event) {
  * @param {Event} event - The triggered event 
  */
 function handleScoreChange(event) {
-    const classes = event.currentTarget.classList;
-    for (let i = 0; i < classes.length; i++) {
-        const cls = classes.item(i);
-        if (cls.startsWith("scorePlayer")) {
-            updateScore(cls);
-        }
+    const input = $(event.target);
+    const playerNums = extractPlayerNums(input);
+
+    for (num of playerNums) {
+        updatePlayerScore(num);
     }
+
     sortPlayersByScore();
+
+    saveGame(input);
 }
 
 /**
- * Update the total score of the player associated with the given class.
- * @param {String} cls - The 'scorePlayerX' class, where X is the player number.
+ * Update the total score of the player associated with the given player number.
+ * @param {String} playerNum - The player number.
  */
-function updateScore(cls) {
-    const playerNum = parseInt(cls.match(/\d+/));
-
+function updatePlayerScore(playerNum) {
     let count = 0;
-    $('.' + cls).each((i, e) => {
+    $('.scorePlayer' + playerNum).each((i, e) => {
         count += strIsBlank(e.value) ? 0 : parseInt(e.value);
     });
 
     $('#playerTotal' + playerNum).text(count);
-}
-
-/**
- * Handle the 'Clear' button being pressed.
- * Make the user confirm so data is not accidentally lost.
- */
-function handleClearButton() {
-    if (confirm("Clear all tournament data?")) {
-        resetData();
-    }
-}
-
-/**
- * Clear all the tournament values.
- */
-function resetData() {
-    $('#divRounds').html('');
-    $('#playerTableBody').html('');
-    $('#numPlayerSelect').change();
-
-    // enable changing number of players
-    $('#numPlayerSelect').prop("disabled", false);
 }
 
 /**
@@ -263,4 +311,51 @@ function sortPlayersByScore() {
 
     tableBody.empty();
     tableBody.append(rows);
+}
+
+/**
+ * Extract the player numbers from the given input's classes.
+ * @param {String} input the input to get the player numbers for.
+ */
+function extractPlayerNums(input) {
+    const nums = [];
+    for (let match of input.attr('class').matchAll(/scorePlayer(\d+)/g)) {
+        nums.push(parseInt(match[1]));
+    }
+    return nums;
+}
+
+/**
+ * Save the game state based on the most recent score change.
+ * @param scoreInput a jquery object of the score input that was just changed.
+ */
+function saveGame(scoreInput) {
+    const roundNum = parseInt(scoreInput.closest('table').data('round'));
+    const playerNum = extractPlayerNums(scoreInput)[0];
+
+    const round = gameData.rounds[roundNum];
+    const pairing = round.find(p => p.players.indexOf(playerNum) != -1);
+    pairing.score = parseInt(scoreInput.val());
+
+    const encodedData = JSON.stringify(gameData);
+    localStorage.setItem("gameData", encodedData);
+}
+
+/**
+ * Load a saved game if it isn't expired.
+ */
+function loadSavedGameData() {
+    const encodedData = localStorage.getItem("gameData");
+    if (encodedData) {
+        const decodedData = JSON.parse(encodedData);
+        if (decodedData && decodedData.players && decodedData.date && decodedData.rounds) {
+            // game data expires after 2 hours
+            const expiration = new Date(decodedData.date);
+            expiration.setHours(expiration.getHours() + 2);
+            if (Date.now() < expiration) {
+                return decodedData;
+            }
+        }
+    }
+    return null;
 }
